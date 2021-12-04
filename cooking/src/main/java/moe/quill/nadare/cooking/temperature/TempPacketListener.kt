c77fabf004cc04c9e3fa117acbaec3af98313fb7
Robert Brunson <siinspam@gmail.com>
2021-12-04 16:30:44 -0500

    freeze packet daemon

--- cooking/src/main/java/moe/quill/nadare/cooking/temperature/TempPacketListener.kt
+++ cooking/src/main/java/moe/quill/nadare/cooking/temperature/TempPacketListener.kt
@@ -1,15 +1,32 @@
 package moe.quill.nadare.cooking.temperature
 
 import com.comphenix.protocol.PacketType
+import com.comphenix.protocol.ProtocolLibrary
 import com.comphenix.protocol.events.ListenerPriority
 import com.comphenix.protocol.events.PacketAdapter
+import com.comphenix.protocol.events.PacketContainer
 import com.comphenix.protocol.events.PacketEvent
 import com.comphenix.protocol.wrappers.WrappedDataWatcher
+import moe.quill.nadare.bukkitcommon.BukkitLambda
+import org.bukkit.Bukkit
+import org.bukkit.entity.Entity
 import org.bukkit.entity.Player
 import org.bukkit.plugin.Plugin
+import java.util.*
 
 class TempPacketListener(plugin: Plugin, private val tempHandler: TempHandler) :
     PacketAdapter(plugin, ListenerPriority.HIGHEST, PacketType.Play.Server.ENTITY_METADATA) {
+
+    init {
+        BukkitLambda {
+            Bukkit.getOnlinePlayers().forEach {
+                ProtocolLibrary.getProtocolManager().sendServerPacket(
+                    it,
+                    createFreezePacket(it, PacketContainer(PacketType.Play.Server.ENTITY_METADATA))
+                )
+            }
+        }.runTaskTimerAsynchronously(plugin, 0, 5)
+    }
 
     override fun onPacketSending(event: PacketEvent?) {
         event ?: return
@@ -18,9 +35,15 @@ class TempPacketListener(plugin: Plugin, private val tempHandler: TempHandler) :
         val entity = event.packet.getEntityModifier(event).read(0) ?: return
         if (entity !is Player) return
 
+        event.packet = createFreezePacket(entity, event.packet)
+    }
+
+    private fun createFreezePacket(entity: Player, raw: PacketContainer): PacketContainer {
         val oldBits = WrappedDataWatcher.getEntityWatcher(entity)
 
         oldBits.setObject(7, tempHandler.getFreezeTicks(entity.uniqueId))
-        event.packet.watchableCollectionModifier.write(0, oldBits.watchableObjects)
+        raw.watchableCollectionModifier.write(0, oldBits.watchableObjects)
+
+        return raw
     }
 }
